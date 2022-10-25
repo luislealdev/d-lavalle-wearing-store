1ca1b34191d213576268290dcc2bbac18efc29b6
Luis Leal <luisrrleal@gmail.com>
2022-10-24 20:31:06 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="w3-center" >
+      <img src="src/assets/img/logo.jpg"/>
+      {/* <h1>PRÓXIMAMENTE</h1> */}
+    </div>
+  );
+}
+
+export default App;
